2ed7c89ea5771793211bf5527c67a41db33ff60b
viniciushtgg <vinicius.gobetti@escola.pr.gov.br>
2025-04-04 07:14:11 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -3,11 +3,30 @@ def jogar():
     print("Bem vindo ao jogo de Forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    acertou = False
+    enforcou = False
+
+    while not acertou and not enforcou:
+        # Pedir pro usuario digitar uma letra
+        chute = input("Qual letra?")
+
+        # Laço for para descobrir se o chute contem
+        # na palavra secreta
+        index = 0
+        for letra in palavra_secreta:
+            if letra == chute:
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            
+            index = index + 1
+
+        print("Jogando...")
 
     print("Fim do jogo")
 
 #Só executa esta parte quando
 # é executado pelo próprio arquivo,
 # ou seja, no forca.py
-if (__name__ == "__main"):
+if (__name__ == "__main__"):
     jogar()
