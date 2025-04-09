472d205be81ad9bac09dac67da6ab73cae40fa7e
viniciushtgg <vinicius.gobetti@escola.pr.gov.br>
2025-04-09 07:25:31 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -4,24 +4,36 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_","_","_","_",]
 
     acertou = False
     enforcou = False
 
+    erros = 0
+
+    print(letras_acertadas)
+
     while not acertou and not enforcou:
         # Pedir pro usuario digitar uma letra
         chute = input("Qual letra?")
+        chute.strip()
 
-        # Laço for para descobrir se o chute contem
-        # na palavra secreta
-        index = 0
-        for letra in palavra_secreta:
-            if letra == chute:
-                print("Encontrei a letra {} na posição {}".format(letra, index))
-            
-            index = index + 1
+        if chute in palavra_secreta :
+            # Laço for para descobrir se o chute contem
+            # na palavra secretab
+            index = 0
+            for letra in palavra_secreta:
+                if letra.upper() == chute.upper():
+                    letras_acertadas[index] = letra
+                
+                index += 1
+        else:
+            # A cada letra errada aumenta o numero de erros
+            erros += 1
 
-        print("Jogando...")
+        # Se o erro for igual a 6, o usuario enforcou
+        enforcou = erros == 6
+        print(letras_acertadas)
 
     print("Fim do jogo")
 
